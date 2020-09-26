687be738e1f146b367231a3c7b13f3162a5c9b0b
Florian Adonis <tanohzana@gmail.com>
2020-09-26 10:50:29 +0000

    Initial boilerplate

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
+
+import HomePage from './pages/home';
+
+import common_fr from './intl/fr.json';
+import common_en from './intl/en.json';
+
+import './index.scss';
+
+i18next.init({
+  interpolation: { escapeValue: false },  // React already does escaping
+  lng: 'fr',                              // language to use
+  resources: {
+    fr: {
+      common: common_fr,
+    },
+    en: {
+      common: common_en,
+    },
+  },
+});
+
+const App = () => {
+  return (
+    <HomePage />
+  );
+};
+
+ReactDOM.render(
+  <I18nextProvider i18n={i18next}>
+    <App/>
+  </I18nextProvider>,
+  document.getElementById('app')
+);
