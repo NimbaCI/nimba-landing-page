59a8a16cdedb3719986fc34f24791f0dec628a88
Florian Adonis <tanohzana@gmail.com>
2022-06-22 22:05:26 +0300

    feat: new landing page and link to connexion

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Toast from 'react-bootstrap/Toast';
-import Spinner from 'react-bootstrap/Spinner';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import { faBolt } from '@fortawesome/free-solid-svg-icons/faBolt';
 import { faHandHoldingUsd } from '@fortawesome/free-solid-svg-icons/faHandHoldingUsd';
 import { faToolbox } from '@fortawesome/free-solid-svg-icons/faToolbox';
 
-import figmaSrc from '../../images/figma.png';
-
 import './Home.scss';
+import Typist from 'react-typist';
 
-const Home = ({ email, errorEmail, handleFormChange, onEmailFormSubmit, isLoading, toggleShowToast, showToast }) => {
+const Home = ({ toggleShowToast, showToast }) => {
   return (
     <div id='home-page'>
       <div className='nav-container'>
@@ -28,53 +25,30 @@ const Home = ({ email, errorEmail, handleFormChange, onEmailFormSubmit, isLoadin
             <Nav className="mr-auto">
             </Nav>
             <Nav>
-              <Nav.Link href="/">Platforme</Nav.Link>
-              <Nav.Link className='disabled-link' disabled href="#">Équipe</Nav.Link>
-              <Nav.Link className='disabled-link' disabled href="#">Prix</Nav.Link>
-              <Nav.Link className='disabled-link' disabled href="#">FAQ</Nav.Link>
+              <Nav.Link className="login-btn" href="https://app.nimba.ci/login">
+                <Button variant="success" type="button">Connexion</Button>
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
       </div>
 
       <div className='section-1'>
-        <h1>Git en <span>Afrique</span> 🌍</h1>
+        <h1>La plateforme <span>GIT</span> <br/> la plus avancée <br/> d'<span>Afrique</span>.</h1>
 
         <h2>Nimba est un <b>client git</b> créé au coeur de l'Afrique, qui s'occupe de la communauté.</h2>
 
-        <div className='form-container'>
-          <p>Nimba est en version alpha, inscrivez-vous pour être tenu au courant.</p>
-
-          <Form className='form' onSubmit={onEmailFormSubmit}>
-            <Form.Row className="align-items-center">
-              <div className="left-input">
-                <Form.Label htmlFor="inlineFormInputEmail" srOnly>
-                  Email
-                </Form.Label>
-                <Form.Control
-                  id="inlineFormInputEmail"
-                  value={email}
-                  isInvalid={errorEmail}
-                  onChange={handleFormChange}
-                  placeholder="jane.doe@example.com"
-                />
-              </div>
-              <div className="right-button">
-                <Button variant="success" type="submit" disabled={isLoading}>
-                  {isLoading ?
-                    <Spinner animation="border" role="status" size="sm">
-                      <span className="sr-only">Chargement...</span>
-                    </Spinner> :
-                    "Je m'inscris !"
-                  }
-                </Button>
-              </div>
-            </Form.Row>
-          </Form>
+        <div className="typist-container">
+          <div className="black-box">
+            <Typist
+              startDelay={1000}
+              cursor={{ show: false }}
+            >
+              <span className="code">npm i -g nimba</span>
+            </Typist>
+          </div>
         </div>
       </div>
-
-      <img src={figmaSrc} className='figma-img' />
 
       <div className='section-2'>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
@@ -83,15 +57,17 @@ const Home = ({ email, errorEmail, handleFormChange, onEmailFormSubmit, isLoadin
 
         <div className='section-2-content'>
           <h2>Un cadeau de l'Afrique pour le monde</h2>
+
           <p className='intro'>La plateforme git la plus avancée du continent</p>
 
           <div className='advantages'>
             <Row>
               <Col sm='6' className='advantages-col'>
-                <div className='inner-column'>
+                <div className='inner-column left-column'>
                   <div className='icon'>
                     <FontAwesomeIcon icon={faGithub} />
                   </div>
+                  <span className='version'>v2</span>
                 </div>
                 <div className='inner-column'>
                   <h3>Migration depuis GitHub ou Gitlab</h3>
@@ -99,10 +75,11 @@ const Home = ({ email, errorEmail, handleFormChange, onEmailFormSubmit, isLoadin
                 </div>
               </Col>
               <Col sm='6' className='advantages-col'>
-                <div className='inner-column'>
+                <div className='inner-column left-column'>
                   <div className='icon'>
                     <FontAwesomeIcon icon={faBolt} />
                   </div>
+                  <span className='version'>v1</span>
                 </div>
                 <div className='inner-column'>
                   <h3>Hébergez votre projet comme partout ailleurs</h3>
@@ -112,10 +89,11 @@ const Home = ({ email, errorEmail, handleFormChange, onEmailFormSubmit, isLoadin
             </Row>
             <Row>
               <Col sm='6' className='advantages-col'>
-                <div className='inner-column'>
+                <div className='inner-column left-column'>
                   <div className='icon'>
                     <FontAwesomeIcon icon={faHandHoldingUsd} />
                   </div>
+                  <span className='version'>v1</span>
                 </div>
                 <div className='inner-column'>
                   <h3>Nous redonnons à la communauté</h3>
@@ -123,10 +101,11 @@ const Home = ({ email, errorEmail, handleFormChange, onEmailFormSubmit, isLoadin
                 </div>
               </Col>
               <Col sm='6' className='advantages-col'>
-                <div className='inner-column'>
+                <div className='inner-column left-column'>
                   <div className='icon'>
                     <FontAwesomeIcon icon={faToolbox} />
                   </div>
+                  <span className='version'>v3</span>
                 </div>
                 <div className='inner-column'>
                   <h3>Connectez-vous avec vos services préférés</h3>
@@ -142,76 +121,12 @@ const Home = ({ email, errorEmail, handleFormChange, onEmailFormSubmit, isLoadin
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
           <path fill="#152715" fill-opacity="1" d="M0,256L80,234.7C160,213,320,171,480,170.7C640,171,800,213,960,240C1120,267,1280,277,1360,282.7L1440,288L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
         </svg>
-
-        <div className='section-3-content'>
-          <div className='grid'>
-            <div className='first-grid-components'>
-              <div className='grid-item grid-item-1'>
-                <div>
-                  <h4>PRODUITS</h4>
-                  <ul>
-                    <li><a href='/'>Platforme</a></li>
-                    <li>Add-ons</li>
-                    <li>Teams</li>
-                    <li>Enterprise</li>
-                  </ul>
-                </div>
-                <div>
-                  <h4>RESSOURCES</h4>
-                  <ul>
-                    <li>Prix</li>
-                    <li>FAQ</li>
-                    <li>Documentation</li>
-                    <li>Développeurs</li>
-                  </ul>
-                </div>
-              </div>
-              <div className='grid-item grid-item-2'>
-                <div>
-                  <h4>ENTREPRISE</h4>
-                  <ul>
-                    <li>À propos</li>
-                    <li>Blog</li>
-                    <li>Recommandations</li>
-                    <li className='help-community'>Aider la communauté 👫</li>
-                  </ul>
-                </div>
-                <div>
-                  <h4>LEGAL</h4>
-                  <ul>
-                    <li>Privacy Policy</li>
-                    <li>Terms of Service</li>
-                    <li>Acceptable Use</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className='grid-item-3'>
-              <h4>INSCRIVEZ-VOUS À NOTRE NEWSLETTER</h4>
-              <p>Les dernières nouvelles, articles et ressources, envoyés chaque semaine dans votre boîte de réception.</p>
-
-              <Form className='newsletter-form'>
-                <Form.Row className="align-items-center">
-                  <div className="left-input">
-                    <Form.Label htmlFor="inlineFormInputEmail" srOnly>
-                      Email
-                    </Form.Label>
-                    <Form.Control id="inlineFormInputEmail" placeholder="jane.doe@example.com" disabled />
-                  </div>
-                  <div className="right-button">
-                    <Button disabled variant="success" type="submit">S'inscrire</Button>
-                  </div>
-                </Form.Row>
-              </Form>
-            </div>
-          </div>
-        </div>
       </div>
 
       <div className='footer'>
         <div className='footer-content'>
           <div>Copyright © Nimba | All right reserved.</div>
-          <div>Fait avec 🧡depuis Abidjan.</div>
+          <div>Fait avec 🧡 depuis Abidjan.</div>
         </div>
       </div>
 
